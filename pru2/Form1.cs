fe81d7e58b49c97f3c8c0d1c91eb01580b46fd4b
David Colmenares <david.colmenares@banlinea.com>
2020-11-05 14:53:15 -0500

    Generacion CSV

--- pru2/Form1.cs
+++ pru2/Form1.cs
@@ -50,28 +50,20 @@ namespace pru2
                         data.Nombre = descripcion.ToString();
                         data.Descripcion = descripcion.ToString();
                         data.Codigo = codigo.ToString();
-                        data.Interrumpe = item.Cell(5).Value.ToString() == "Rechazo" ? true : false;
+                        data.Mandatorio = item.Cell(5).Value.ToString() == "Rechazo" ? true : false;
                         var pasos = item.Cell(7).Value;
                         data.MensajeError = item.Cell(8).Value.ToString();
                         var xpath = item.Cell(13).Value.ToString();
                         xpath = xpath.Substring(1);
                         string[] stringSeparators = new string[] { "\n" };
                         string[] lines = pasos.ToString().Split(stringSeparators, StringSplitOptions.None);
-                        data.XPATH = "{textBox2.Text}ApplicationResponse/cac:DocumentResponse/cac:Response/cbc:ResponseCode/text() = '043'";
+                        var xpathGnral = item.Cell(12).Value.ToString();
+                        data.XPATH = $"{(string.IsNullOrEmpty(textBox2.Text) ? "/" : textBox2.Text)}{xpathGnral.Substring(1)}";
                         foreach (var det in lines)
-                        {
+                        { 
                             var obj = det.Replace("\r", "");
                             switch (obj)
-                            {
-                                case string a when a.Contains("Rechazo"):
-                                    data.Interrumpe = true;
-                                    break;
-                                case string c when c.Contains("Opcional"):
-                                    data.Mandatorio = false;
-                                    break;
-                                case string d when d.Contains("Obligatorio"):
-                                    data.Mandatorio = true;
-                                    break;
+                            { 
                                 case string g when g.Contains("numeral"):
                                     data.Tipo = "En lista";
                                     data.CondicionXPATH.Add($"{textBox2.Text}{xpath}");
@@ -91,7 +83,7 @@ namespace pru2
                                     text = text.Replace("“", "");
                                     text = text.Replace("”", "");
                                     text = text.Replace("\"", "");
-                                    data.CondicionXPATH.Add($"{textBox2.Text}{xpath}/text()= '{text}'");
+                                    data.CondicionXPATH.Add($"{textBox2.Text}{xpath} = '{text}'");
                                     break;
                                 case string b when b.Contains("ocurrencia"):
                                     var ocurrencia = Regex.Match(obj.Substring(2), @"\d+").Value;
@@ -110,7 +102,15 @@ namespace pru2
                                     data.CondicionXPATH.Add($"count({textBox2.Text}{xpath})= {ocurrencia}");
                                     break;
                                 case string i when i.Contains("vacío"):
+                                case string k when k.Contains("vacio"):
                                     data.CondicionXPATH.Add($"string-length({textBox2.Text}{xpath})> {0}");
+                                    break;
+                                case string m when m.Contains("vacíoG"):
+                                case string n when n.Contains("vacioG"):
+                                    data.CondicionXPATH.Add($"count({textBox2.Text}{xpath}/*)> {0}");
+                                    break;
+                                case string l when l.Contains("exista"):
+                                    data.CondicionXPATH.Add($"exists({textBox2.Text}{xpath})");
                                     break;
                                 default:
                                     data.CondicionXPATH.Add($"No c:{obj}");
@@ -121,6 +121,31 @@ namespace pru2
                         lst.Add(data);
                     }
                     textBox1.Text = JsonConvert.SerializeObject(lst);
+
+                    string fl = "PartitionKey, RowKey, Timestamp, Active, Active@type,BreakOut,BreakOut @type, Category, Category@type,Created,Created @type, CreatedBy, CreatedBy@type,Deleted,Deleted @type, Description, Description@type,DocumentTypeCode,DocumentTypeCode @type, ErrorCode, ErrorCode@type,ErrorMessage,ErrorMessage @type, Mandatory, Mandatory@type,Name,Name @type, Priority, Priority@type,RuleId,RuleId @type, Type, Type@type,TypeListId,TypeListId @type, Updated, Updated@type,UpdatedBy,UpdatedBy @type, XPath, XPath@type,XpathEvaluation,XpathEvaluation @type";
+                    var csv = new StringBuilder();
+                    csv.AppendLine(fl);
+
+                    foreach (var item in lst)
+                    {
+                        if (item.Tipo != "En lista")
+                        {
+
+                            var d = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'");
+                            Guid g = Guid.NewGuid();
+                            var data = $"\"{item.CondicionXPATH.Aggregate((s, s1) => s + " AND " + s1).ToString()} \"";
+                            string line = $"new-dian-ubl21,{g},{d},true,Edm.Boolean,false,Edm.Boolean,new-dian-ubl21,Edm.String," +
+                                          $"{d},Edm.DateTime,ldcolmenares@indracompany.com,Edm.String,false,Edm.Boolean,{item.Descripcion?.Replace(",", " ")}," +
+                                          $"Edm.String,96,Edm.String,{item.Codigo},Edm.String,{item.MensajeError.Replace(",", " ")},Edm.String," +
+                                          $"{item.Mandatorio},Edm.Boolean,{item.Descripcion?.Replace(",", " ")},Edm.String,0,Edm.Int32,{g},Edm.Guid,"+
+                                          $"0,Edm.Int32,,,{d},Edm.DateTime,ldcolmenares@indracompany.com,Edm.String" +
+                                          $",{data}" +
+                                          $",Edm.String,{item.XPATH},Edm.String";
+                            csv.AppendLine(line); 
+                        }
+                    }
+                    File.WriteAllText($"{file}Result.csv", csv.ToString(), Encoding.Default);
+
 
                 }
             }
